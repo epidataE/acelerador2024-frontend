1a8c924584fe45800a2b60ef65bb47d62732e67c
Bianca Stork <biancastrk@gmail.com>
2024-09-14 00:42:41 -0300

    Ver Equipos y Participantes

--- src/componentes/EquipoList.js
+++ src/componentes/EquipoList.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 const EquipoList = () => {
     const [equipos, setEquipos] = useState([]);
+    
 
     useEffect(() => {
         fetch('/api/equipos')
             .then(response => response.json())
-            .then(data => setEquipos(data));
+            .then(data =>  setEquipos(data));
     }, []);
 
     return (
@@ -21,8 +22,8 @@ const EquipoList = () => {
                         <p>Curso: {equipo.curso}</p>
                         <p>Participantes:</p>
                         <ul>
-                            {equipo.users.map(user => (
-                                <li key={user.id}>{user.nombre} {user.apellido}</li>
+                            {equipo.usuarios.map(user => (
+                                <li key={user.id}>{user.nombre} {user.apellido} | {user.rol} | {user.especializacion}</li>
                             ))}
                         </ul>
                     </div>
